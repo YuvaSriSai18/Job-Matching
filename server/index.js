06ff625412cd1e79f1439d891cccd0903935c057
Yuva Sri Sai <nanithota18102004@gmail.com>
2024-10-16 01:03:46 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:[ "http://localhost:5173" , "https://job-matching-18.vercel.app"],
     methods: "GET,POST,PUT,DELETE",
   })
 );
